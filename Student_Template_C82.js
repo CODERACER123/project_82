765ee2b2ca38475f80723981005d47c15fcd1246
CODERACER123 <99765463+CODERACER123@users.noreply.github.com>
2023-05-03 17:49:22 -0400

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,18 @@
+console.log("My first console project");
+
+            var first_name ="Mritunjai";
+            console.log(first_name);
+            var last_name ="Prashanth";
+            console.log(last_name);
+            var full_name=first_name.concat(last_name);
+            console.log(full_name);
+            var a = 10;
+            var b = 5;
+            var add =a+b;
+            console.log(add );
+             var sub = a-b;
+            console.log(sub);
+            var multiple =a*b;
+            console.log(multiple);
+            var divide = a / b;
+            console.log(divide);
